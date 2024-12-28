717587e1c36c5b37f1474bbae6c490124b0a4119
xinhongbing <xinhongbing@jxtech.net>
2024-12-28 23:49:32 +0800

    NavigationBar

--- src/pages/chat/chat.vue
+++ src/pages/chat/chat.vue
@@ -4,4 +4,9 @@
 
 <script setup lang="ts">
 import AIMessage from "@/components/AIMessage.vue";
+
+onNavigationBarButtonTap((obj) => {
+  // 监听导航栏按钮点击
+  console.log('onNavigationBarButtonTap', obj)
+})
 </script>
